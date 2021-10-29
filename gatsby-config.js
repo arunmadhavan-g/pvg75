bc9a154e6abb14e7c5a7e8cdadc5ae2f7f34ab47
arunmadhavan-g <arunmadhavan.g@gmail.com>
2021-10-29 10:51:33 +0530

    Adds favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,23 @@
 module.exports = {
-  pathPrefix: '/pvg75',
-  plugins: [
-    {
-      resolve: 'gatsby-theme-photo-albums',
-      options: {
-        folderIconDir: 'folder-icon',
-        photosPerPage: 100,
-      },
-    },
-  ],
+    pathPrefix: '/pvg75',
+    plugins: [{
+            resolve: 'gatsby-theme-photo-albums',
+            options: {
+                folderIconDir: 'folder-icon',
+                photosPerPage: 100,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `PVG75`,
+                short_name: `PVG75`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/images/favicon.png`
+            },
+        },
+    ],
 }
